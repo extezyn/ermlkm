fd7644bb508d8f73a3e66386960723400406361c
extezyn <144113053+extezyn@users.noreply.github.com>
2024-03-16 04:40:47 +0300

    sad

---
+++ ZachemYaZhivu/Page3.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZachemYaZhivu
+{
+    /// <summary>
+    /// Логика взаимодействия для Page3.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        private FitnessClubEntities fit = new FitnessClubEntities();
+        public Page3()
+        {
+            InitializeComponent();
+            getgrid.ItemsSource = fit.Members.ToList();
+        }
+    }
+}
